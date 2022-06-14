21c9274fa578493a1f8e4b39933ae1e519ea2137
John Doe <sailalith1993@gmail.com>
2022-06-14 13:12:08 +0530

    apply in Js

--- Js_window.js
+++ Js_window.js
@@ -4,5 +4,6 @@ var addToThis = function(a, b, c) {
     return this.num + a + b + c;
     
 };
-console.log(addToThis.call(obj,1,2,3));
-    
+//console.log(addToThis.call(obj,1,2,3));
+var arr = [1,2,3];
+console.log(addToThis.apply(obj,arr));
