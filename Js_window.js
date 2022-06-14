2938366f08a2e241bcce54f490486e1a55fcbdb4
John Doe <sailalith1993@gmail.com>
2022-06-14 13:42:13 +0530

    2 methods of currying

--- Js_window.js
+++ Js_window.js
@@ -10,12 +10,34 @@
 
 // console.log(bound(1,2,3));
 
-Student = {age: 20};
+// Student = {age: 20};
 
-var logAge = function(){
-    console.log(this.age);
-};
+// var logAge = function(){
+//     console.log(this.age);
+// };
 
-printAge = logAge.bind(Student);
+// printAge = logAge.bind(Student);
 
-printAge();
+// printAge();
+
+// fUNCTION currying 1st method
+// let multiply = function(x, y){
+//     console.log(x * y);
+// };
+// let multiplyByTwo = multiply.bind(this,2);
+// multiplyByTwo(5);
+
+// let multiplyByThree = multiply.bind(this, 3);
+// multiplyByThree(6);
+// fUNCTION currying 2nd method
+let multiply = function(x){
+    return function(y){
+        console.log(x*y);
+    }
+}
+
+let multiplyByTwo = multiply(2);
+multiplyByTwo(3);
+
+let multiplyByThree = multiply(3);
+multiplyByThree(3);
