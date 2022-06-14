c36f8c712821ab7176bc97cf900194358c6d019c
John Doe <sailalith1993@gmail.com>
2022-06-14 13:19:53 +0530

    bind in Js

--- Js_window.js
+++ Js_window.js
@@ -6,4 +6,8 @@ var addToThis = function(a, b, c) {
 };
 //console.log(addToThis.call(obj,1,2,3));
 var arr = [1,2,3];
-console.log(addToThis.apply(obj,arr));
+var bound = addToThis.bind(obj);
+
+console.log(bound(1,2,3));
+
+
